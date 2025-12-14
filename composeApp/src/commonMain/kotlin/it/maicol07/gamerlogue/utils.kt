ee69b578fbb977badcb69b87e3ee000eaada553d
Maicol Battistini <maicolbattistini@live.it>
2025-12-14 13:13:35 +0100

    refactor: 🤫 Ignore cancellation exceptions in error reporting
    Prevents `CancellationException` from being reported as a network error within the `safeRequest` utility function. This avoids treating user-initiated cancellations (like navigating away) as actual failures.

--- composeApp/src/commonMain/kotlin/it/maicol07/gamerlogue/utils.kt
+++ composeApp/src/commonMain/kotlin/it/maicol07/gamerlogue/utils.kt
@@ -4,6 +4,7 @@ import at.released.igdbclient.error.IgdbException
 import com.github.michaelbull.result.runCatching
 import com.github.michaelbull.result.unwrapError
 import it.maicol07.gamerlogue.ui.components.layout.AppUi
+import kotlin.coroutines.cancellation.CancellationException
 
 suspend fun <T> safeRequest(request: suspend () -> T) = runCatching {
     request()
@@ -12,7 +13,10 @@ suspend fun <T> safeRequest(request: suspend () -> T) = runCatching {
         val error = unwrapError()
         when (error) {
             is IgdbException -> {
-                AppUi.reportNetworkException(error)
+                // Do not report cancellation exceptions
+                if (error.cause !is CancellationException) {
+                    AppUi.reportNetworkException(error)
+                }
             }
         }
         error.printStackTrace()
