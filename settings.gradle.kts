6cc827202424f72902ef0ba1b13d91296208a4a7
Maicol Battistini <maicolbattistini@live.it>
2026-01-05 14:54:06 +0100

    build: 🔧 Add Sonatype snapshots repository
    Adds the Sonatype snapshots repository (`https://central.sonatype.com/repository/maven-snapshots/`) to the project's dependency resolution management.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+import java.net.URI
+
 rootProject.name = "Gamerlogue"
 
 pluginManagement {
@@ -30,6 +32,11 @@ dependencyResolutionManagement {
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev/")
         maven("https://maven.universablockchain.com/")
         maven("https://jitpack.io/")
+
+        maven {
+            name = "Central Portal Snapshots"
+            url = URI("https://central.sonatype.com/repository/maven-snapshots/")
+        }
     }
 }
 include(":composeApp")
