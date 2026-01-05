832c005706f8663984b5a4aa2bc39a22c1217f07
Maicol Battistini <maicolbattistini@live.it>
2026-01-05 15:02:21 +0100

    refactor: 🤫 Mark network exception as volatile
    Marks the `networkException` variable in `AppUiState` with the `@Volatile` annotation. This ensures that changes to its value are always visible to all threads, preventing potential concurrency issues.

--- composeApp/src/commonMain/kotlin/it/maicol07/gamerlogue/ui/components/layout/AppUiState.kt
+++ composeApp/src/commonMain/kotlin/it/maicol07/gamerlogue/ui/components/layout/AppUiState.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.staticCompositionLocalOf
+import kotlin.concurrent.Volatile
 
 class AppUiState {
     var networkException = mutableStateOf<Throwable?>(null)
